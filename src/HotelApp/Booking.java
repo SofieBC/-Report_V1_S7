aba3e4771e21d08f7c91fe0f5d8febdbbe299116
Lukkah123 <h_lukka@hotmail.com>
2019-11-21 15:08:35 +0100

    Version 2

---
+++ src/HotelApp/Booking.java
@@ -0,0 +1,15 @@
+package HotelApp;
+
+import java.util.Date;
+
+public class Booking {
+    private int bookingId;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private double totalPrice;
+
+    public Booking(int bookingId, Date checkInDate) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+    }
+}
