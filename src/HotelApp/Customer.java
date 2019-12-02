1742dfde940662a3584d3896b5aaaf6f0f988d32
SofieBC <fikonglass@hotmail.com>
2019-12-02 16:26:59 +0100

    sofies ändringar

--- src/HotelApp/Customer.java
+++ src/HotelApp/Customer.java
@@ -1,15 +1,29 @@
 package HotelApp;
 
+import java.util.ArrayList;
+
 public class Customer {
     private String ssn;
     private String name;
-    private String adress;
+    private String address;
     private String telephoneNumber;
 
     public Customer(String ssn, String name, String adress, String telephoneNumber) {
         this.ssn = ssn;
         this.name = name;
-        this.adress = adress;
+        this.address = adress;
         this.telephoneNumber = telephoneNumber;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "[Customer: " +
+                "ssn='" + ssn + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", telephoneNumber='" + telephoneNumber + '\'' +
+                ']' + "\n";
     }
 }
--- src/HotelApp/HotelApp.java
+++ src/HotelApp/HotelApp.java
@@ -1,52 +1,61 @@
 package HotelApp;
 
+import java.util.Scanner;
+
+
 public class HotelApp {
-    private Scanner input = new Scanner(System.in);
+    public static void main (String[]args) {
 
-    public void runProgram() {
-        boolean running = true;
-        while (running) {
-            int userMenuChoice = printMenu();
-            switch (userMenuChoice) {
-                case 1:
-                    //Method
-                    break;
-                case 2:
-                    //Metod
-                    break;
-                case 3:
-                    //Method
-                    break;
-                case 4:
-                    //Method
-                    break;
-                case 5
-                    ;
-                    running = false;
-                    System.out.println("Thank you for using the services");
-                    break;
-                default:
-                    System.out.println("Enter a proper value");
-                    break;
+        HotelLogic logic = new HotelLogic ();
+
+        logic.getCustomer();
+        Scanner input = new Scanner (System.in);
+
+       /* public void runProgram () {
+            boolean running = true;
+            while (running) {
+                int userMenuChoice = printMenu ();
+                switch (userMenuChoice) {
+                    case 1:
+                        //Method
+                        break;
+                    case 2:
+                        //Metod
+                        break;
+                    case 3:
+                        //Method
+                        break;
+                    case 4:
+                        //Method
+                        break;
+                    case 5
+                        ;
+                        running = false;
+                        System.out.println ("Thank you for using the services");
+                        break;
+                    default:
+                        System.out.println ("Enter a proper value");
+                        break;
+                }
+
+
+                HotelApp myApp = new HotelApp ();
+                HotelLogic logic = new HotelLogic ();
+                logic.checkInCustomer ("555555-555", true);
+
+                Customer customer1 = new Customer ("55555", "kalle", "hejvägen12", "07777");
+
+                myApp.printMenu ();
             }
 
-            public static void main (String[]args){
 
-                HotelApp myApp = new HotelApp();
-                HotelLogic logic = new HotelLogic();
-                logic.checkInCustomer("555555-555", true);
-
-                Customer customer1 = new Customer("55555", "kalle", "hejvägen12", "07777");
-
-                myApp.printMenu();
-            }
-
-
-        }
+        }*/
 
     }
 
     public int printMenu() {
+
+        Scanner input = new Scanner(System.in);
         System.out.println("*______ Welcome ______*");
         System.out.println("| 1) Add booking      |");
         System.out.println("| 2) Cancel booking   |");
